1886ee438a378ce697acdf4226bf8549b3bd8c9f
Sara Wang <xywong23@gmail.com>
2019-06-26 19:30:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,8 +13,11 @@
 # 'I LOVE YOU GRANDMA!'
 
 def speak_to_grandma(string)
-  if "string" == "I LOVE YOU GRANDMA!"
+  if string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+  elsif string == "string".upcase
+    puts ""
+  
   end
 end
   
